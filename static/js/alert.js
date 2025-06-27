9df2d33e72e84f99c73f73036b93617661f77c56
dhruv108-maker <135990735+dhruv108-maker@users.noreply.github.com>
2025-06-27 18:45:28 +0530

    updated Index1.html

--- static/js/alert.js
+++
@@ -1,9 +0,0 @@
-// Automatically hide alerts after 5 seconds
-document.addEventListener('DOMContentLoaded', () => {
-    const alerts = document.querySelectorAll('.alert');
-    alerts.forEach(alert => {
-        setTimeout(() => {
-            alert.style.display = 'none';
-        }, 5000); // 5 seconds
-    });
-});
